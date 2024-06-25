646750a496305745e54b9e50314cb5aa3e996d89
makoge boris <makogeboris@gmail.com>
2024-06-25 09:57:24 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const errorMessage = document.querySelectorAll(".form__error");
+
+for (const error of errorMessage) {
+  error.classList.remove("form__error");
+  // error.textContent = "This field is required";
+}
+
+// This field is required
+// Please enter a valid email address
+// Please select a query type
+// To submit this form, please consent to being contacted
+// Message Sent!
+// Thanks for completing the form. We'll be in touch soon!
