4b2f7cca53545f727596e954698f1ad0b8089503
makoge boris <makogeboris@gmail.com>
2024-07-08 16:53:47 +0100

    improvements

--- script.js
+++ script.js
@@ -89,3 +89,16 @@ formGroups.forEach((group) => {
     textarea.addEventListener("focus", clearError);
   }
 });
+
+const radioGroups = document.querySelectorAll(".form__radio-group-wrap");
+
+radioGroups.forEach((group) => {
+  const radio = group.querySelector(".form__radio");
+
+  radio.addEventListener("change", () => {
+    radioGroups.forEach((g) => (g.style.backgroundColor = ""));
+    if (radio.checked) {
+      group.style.backgroundColor = "var(--LightGreen)";
+    }
+  });
+});
